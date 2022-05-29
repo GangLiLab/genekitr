1e2dd77f6c181a11e0bc37a7dff480a2e9630bd0
reedliu <jieandze1314@gmail.com>
2022-05-29 11:37:57 +0800

    add new file

--- R/transID.R
+++ R/transID.R
@@ -3,6 +3,7 @@
 #' @param transTo Transform to what type. User could select one or more from
 #' "symbol", "entrez", "ensembl" or "uniprot."
 #' @param org Latin organism shortname from `ensOrg_name`. Default is human.
+#' @param unique Logical, if one-to-many mapping occurs, only keep one record with fewest NA. Default is FALSE.
 #' @param keepNA If some id has no match at all, keep it or not. Default is FALSE.
 #' @importFrom dplyr filter pull select distinct arrange all_of filter_at vars any_vars
 #' @importFrom rlang .data
@@ -31,6 +32,7 @@
 transId <- function(id,
                     transTo,
                     org = 'hs' ,
+                    unique = FALSE,
                     keepNA = FALSE) {
 
   #--- args ---#
@@ -53,7 +55,7 @@ transId <- function(id,
   }
 
   #--- codes ---#
-  res <- genInfo(id, org, unique = FALSE, keepNA) %>%
+  res <- genInfo(id, org, unique, keepNA) %>%
     dplyr::select(input_id, all_of(transTo)) %>%
     distinct()
   if (!keepNA) {
