95861ae438a5ae52eac3b3cb449e2b192e0bb493
reedliu <jieandze1314@gmail.com>
2022-06-09 14:36:47 +0800

    add pubmed trend function

--- R/getPubmed.R
+++ R/getPubmed.R
@@ -36,7 +36,27 @@ getPubmed <- function(term,add_term=NULL,num=100){
 
 
 
-#---BELOW DEGRADED! ---#
+#--- sub function for plotting ---#
+getPubmedTrend <- function(term,add_term=NULL,period){
+  if (!requireNamespace("europepmc", quietly = TRUE)) {
+    utils::install.packages("europepmc")
+  }
+  if(!is.null(add_term)){
+    supp = paste0('AND ABSTRACT:',add_term) %>% paste(collapse = ' ')
+  }else{
+    supp = NULL
+  }
+
+  res = lapply(term , function(i){
+    europepmc::epmc_hits_trend(paste0('ABSTRACT:',i," ",supp), period = period, synonym = FALSE)
+  })
+  names(res) = term
+
+  return(res)
+}
+
+
+#--- DEPRECATED---#
 # genPubmed <- function(id,
 #                       keywords,
 #                       field = "ALL") {
