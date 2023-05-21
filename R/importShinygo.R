25ff7e875f7edc8be8a331bb53c61e47aefbc9eb
reedliu <jieandze1314@gmail.com>
2023-05-21 11:14:16 +0800

    add new file

---
+++ R/importShinygo.R
@@ -0,0 +1,34 @@
+#' Import 'shinyGO' web result
+#'
+#' @param shinygo_file ShinyGO result file.
+#'
+#' @importFrom dplyr select mutate relocate
+#' @importFrom stats setNames
+#' @importFrom rlang .data
+#' @return  `data.frame`
+#' @export
+
+importShinygo <- function(shinygo_file) {
+  if (!requireNamespace("rio", quietly = TRUE)) {
+    utils::install.packages("rio")
+  }
+
+  shinygo <- rio::import(shinygo_file)
+
+  dat <- shinygo %>%
+    dplyr::select(c('Ontology Code','Pathway','nGenes','Pathway Genes','Enrichment FDR','Genes','Fold Enrichment')) %>%
+    stats::setNames(c('ID','Description','Count','Pathway_count','qvalue','geneID','FoldEnrich')) %>%
+    dplyr::mutate(RichFactor = Count/Pathway_count) %>%
+    dplyr::relocate(RichFactor,.after = FoldEnrich) %>%
+    dplyr::relocate(Count,.after = geneID) %>%
+    dplyr::select(-Pathway_count) %>%
+    dplyr::mutate(geneID = gsub('\\s+','/',geneID) ) %>%
+    dplyr::mutate(FoldEnrich = as.numeric(FoldEnrich)) %>%
+    dplyr::mutate(qvalue = as.numeric(qvalue))
+
+  return(dat)
+}
+
+utils::globalVariables(c(
+  "Pathway_count", "RichFactor"
+))
